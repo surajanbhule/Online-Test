3874c4d5f46d98a3650b8824f336db08186e40f4
Suraj Anbhule <surajanbhule69@gmail.com>
2022-10-09 11:08:02 +0530

    Database Configuration - Added some models.
    models - User, UserRole, Role is added

---
+++ Online-Test/src/main/java/com/onlinetest/app/models/Role.java
@@ -0,0 +1,46 @@
+package com.onlinetest.app.models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String roleName;
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "role")
+    private Set<UserRole> userRoles=new HashSet<>();
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+
+}
