e28ac8be0df2e7999f5537a0adcdd9d2f797bce9
Suraj Anbhule <surajanbhule69@gmail.com>
2022-10-12 18:29:49 +0530

    Frontend - login generating token feature added token generation function added to login component

--- online-test-front/src/app/pages/login/login.component.ts
+++ online-test-front/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData={
+    username:'',
+    password:''
+  }
+  constructor(private loginService:LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit(){
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data)=>{
+        console.log("Success");
+        console.log(data);
+      },
+      (error)=>{
+        console.log("error");
+        console.log(error);
+      }
+    )
+  }
+
+  public loginUser(token:string){
+    localStorage.setItem('userToken',token);
+    return true;
+  }
+
+  public isLoggedIn(){
+    let tokenStr=localStorage.getItem('userToken');
+    if(tokenStr==undefined || tokenStr=='' || tokenStr==null){
+      return false;
+    }else{
+      return true;
+    }
+    
+  }
+
+  public logout(){
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('user');
+    return true;
+  }
+
+  public getToken(){
+    return localStorage.getItem('userToken');
+  }
+
+  public setUser(user:any){
+    localStorage.setItem('user',JSON.stringify(user));
+
+  }
+
+  public getUser(){
+    let userStr=localStorage.getItem('user');
+    if(userStr!=null){
+      return JSON.parse(userStr);
+    }else{
+      this.logout();
+      return false;
+    }
+  }
+
+  public getUserRole(){
+    let user= this.getUser();
+    return user.authorities[0].authority;
   }
 
 }
